ead21022753858f8be06f2a1d86d039b605f363f
JJNCreator <jakebayerct@gmail.com>
2019-11-11 18:01:54 -0500

    Initial
    Initial

---
+++ Made You Look sample/Assets/Scripts/PowerOutlet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerOutlet : MonoBehaviour {
+
+	private void OnMouseUp()
+	{
+		if(Vector3.Distance(RoomManager.Instance.playerCharacter.transform.position, transform.position) < 1.5f) {
+			if(InventoryManager.Instance.InventoryContains("Power Cord")) {
+				RoomManager.Instance.hasPluggedIn = true;
+				RoomManager.Instance.secondPowerCord.SetActive(true);
+				InventoryManager.Instance.RemoveInventoryItem("Power Cord", InventoryButton.ItemType.None);
+				InventoryManager.Instance.RemoveInventoryItem("X-Ray Goggles", InventoryButton.ItemType.None);
+				RoomManager.Instance.UpdateRugTransparency();
+			}
+			else {
+				RoomManager.Instance.PlayErrorSound();
+			}
+		}
+	}
+}
---
+++ Made You Look sample/Assets/Scripts/Safe.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : MonoBehaviour {
+	public bool usesKey = false;
+
+	private Animator ani;
+	private AudioSource audioS;
+
+	private void Awake()
+	{
+		ani = GetComponent<Animator>();
+		audioS = GetComponent<AudioSource>();
+	}
+
+	private void OnMouseUp()
+	{
+		if(Vector3.Distance(RoomManager.Instance.playerCharacter.transform.position, transform.position) < 1.5f) {
+			if(usesKey) {
+				if (InventoryManager.Instance.InventoryContains("Key"))
+                {
+                    ani.SetTrigger("OpenSafe");
+                    audioS.Play();
+                    InventoryManager.Instance.RemoveInventoryItem("Key", InventoryButton.ItemType.None);
+                    RoomManager.Instance.hasOpenedSafe = true;
+                }
+				else
+				{
+					RoomManager.Instance.PlayErrorSound();
+				}
+			}
+		}
+	}
+	public void OpenSafeViaPasscode() {
+		ani.SetTrigger("OpenSafe");
+		audioS.Play();
+		RoomManager.Instance.hasOpenedSafeInRoomThree = true;
+	}
+}
